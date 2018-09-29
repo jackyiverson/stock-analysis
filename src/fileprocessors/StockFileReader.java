b3616064416e36fe9b5e5076fd0eb565e7a9024e
Pei Fen Lo <kuasipccv@gmail.com>
2018-09-30 02:38:38 +0800

    a little bug

---
+++ src/fileprocessors/StockFileReader.java
@@ -0,0 +1,56 @@
+package fileprocessors;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StockFileReader {
+	
+	String filePath = null;
+	
+	public StockFileReader(String filePath){
+		this.filePath = filePath;
+	}
+	
+	public List<String> getHeaders() throws IOException{
+		String line = readFirstLine(filePath);
+		String [] header = line.split(",");
+		List<String> values = new ArrayList<String>();
+		values = Arrays.asList(header);
+		return values;
+	}
+	
+	static String readFirstLine(String path) throws IOException {
+	    try (BufferedReader br =
+	                   new BufferedReader(new FileReader(path))) {
+	        return br.readLine();
+	    }
+	    
+	}
+	/**
+	 * Complete the body of this method.
+	 * @return List
+	 * @throws IOException
+	 */
+	public List<String> readFileData() throws IOException{
+		List<String> lines = new ArrayList<String>();
+		// Insert your code here..
+		try(BufferedReader br = new BufferedReader(new FileReader(filePath))){
+			String line = br.readLine();
+			// ignore first headLine
+			line=br.readLine();
+			while(line!=null) {
+//				System.out.println(line);
+				lines.add(line);
+				line=br.readLine();
+			}
+		}
+	    return lines;
+	}
+	
+
+}
